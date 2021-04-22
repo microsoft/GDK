81dd35a360f1361ec15bfc5e6f9f559bab67b460
Clint Woon <clintw@microsoft.com>
2021-04-22 09:13:00 -0700

    Microsoft Public April 2021 GDK GA RELEASE [19041.6078.210415-2100]

---
+++ Installers/InstallGamingServicesBundle.ps1
@@ -0,0 +1,138 @@
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory=$false)]
+    [string]
+    $AppxPath,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $LogPath,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $AppxDepDirPath
+)
+
+function Logic
+{
+   $Error.Clear();
+
+   if(!$AppxPath)
+   {
+       $AppxPath = Join-Path $PSScriptRoot "GamingServices.appxbundle";
+   }
+
+    if(-not (test-path $AppxPath))
+   {
+       Write-Output "ERROR: Appx not found: $AppxPath";
+       exit 1;
+   }
+   
+   if(!$AppxDepDirPath)
+   {
+       $AppxDepDirPath = (get-item $AppxPath).Directory.FullName;
+   }
+
+   $systemVolume = Get-AppxVolume | Where-Object -Property IsSystemVolume -eq $True | Select-Object -First 1;
+
+   if(!$systemVolume)
+   {
+       Write-Output "ERROR: System volume not found.";
+       exit 1;
+   }
+
+   Write-Output ("System volume found.`n{0}" -f ($systemVolume | Format-List | Out-String).trim());
+
+   $systemRoot = [System.IO.path]::GetPathRoot($systemVolume.PackageStorePath);
+
+   if(!$systemRoot)
+   {
+       Write-Output "ERROR: System root not found.";
+       exit 1;
+   }
+
+   Write-Output "System root found: $systemRoot";
+
+   $package = Get-AppxPackage -Name Microsoft.GamingServices* | Sort-Object -Property Version -Descending | Select-Object -First 1;
+   $isOldPackagePresent = $false;
+
+   if($package)
+   {
+       Write-Output ("Version {0} of {1} was found already installed on the PC.`n{2}" -f $package.Version, $package.Name, ($package | Format-List | Out-String).trim());
+
+       $isOldPackagePresent = (([Version]$package.Version -lt [Version]'2.52.6001.0') -or ([Version]$package.Version -eq [Version]'1.43.2002.0'));
+
+       if($isOldPackagePresent)
+       {
+           Write-Output "Uninstalling $($package.PackageFullName) version $($package.Version)";
+           remove-appxpackage -allusers $package.PackageFullName -ErrorAction Continue;
+
+           if($Error)
+           {
+               Write-Output "Remove-AppxPackage failed for $($package.PackageFullName)";
+           }
+           else
+           {
+               Write-Output "Remove-AppxPackage completed for $AppxPath";
+           }
+       }
+   }
+   else
+   {
+       Write-Output 'No versions of Microsoft.GamingServices were found installed on the PC.';
+   }
+
+   if(($package -eq $null) -or $isOldPackagePresent)
+   {
+       Write-Output "Installing $AppxPath Version 2.52.6001.0 Volume $systemRoot";
+
+       $allDepsFound = $TRUE;
+       [string[]]$depPackageNames = 'Microsoft.NET.Native.Runtime.2.2','Microsoft.NET.Native.Framework.2.2','Microsoft.VCLibs.x64.14.00';
+       $dependencyPaths = $depPackageNames | ForEach-Object {Join-Path -Path $AppxDepDirPath -ChildPath "$_.appx"};
+       ForEach ($dep in $dependencyPaths) {if (-not (test-path $dep)) {$allDepsFound = $FALSE;}};
+
+       if($allDepsFound)
+       {
+           Write-Output "Installing $AppxPath Version 2.52.6001.0 Volume $systemRoot -DependencyPath $dependencyPaths";
+           add-appxpackage -forceApplicationShutdown -path $AppxPath -Volume $systemRoot -DependencyPath $dependencyPaths -ErrorAction Continue;
+       }
+       else
+       {
+           Write-Output "Installing $AppxPath Version 2.52.6001.0 Volume $systemRoot";
+           add-appxpackage -forceApplicationShutdown -path $AppxPath -Volume $systemRoot -ErrorAction Continue;
+       }
+
+       if($Error)
+       {
+           Write-Output "ERROR: Add-AppxPackage failed for $AppxPath";
+           $guid = ($error -split ' ' | select -Last 1) -as [Guid];
+           if($guid)
+           {
+               Write-Output "Get-AppPackageLog -ActivityID $guid";
+               Get-AppPackageLog -ActivityID $guid;
+           }
+       }
+       else
+       {
+           Write-Output "Add-AppxPackage completed for $AppxPath";
+       }
+   }
+   else
+   {
+       Write-Output "No installation needed as version $($package.Version) of $($package.Name) was found already installed on the PC.";
+   }
+}
+
+$output = Logic;
+
+if($LogPath)
+{
+    $output | Out-File -FilePath $LogPath -Append -Force;
+}
+else
+{
+    $output;
+}
+
+exit ($Error.Count);
